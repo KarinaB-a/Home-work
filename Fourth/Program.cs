7b3a5f9ca44ff7a066da169492bead20fc909a78
Karina Babakova <taytynovak@mail.ru>
2022-09-04 23:49:21 +0700

    Задача на получение четных чисел

---
+++ Fourth/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите число ");
+int N = int.Parse(Console.ReadLine()); 
+Console.WriteLine($"Все четные числа от 1 до {N}:");
+for (int A = 1; A <= N; A++)
+{
+    if(A%2 == 0)
+    {
+        Console.Write($"{A} ");
+    }
+}
