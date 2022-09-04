80934e74f6fa90a248c5b3cd908d330c556a9de9
Karina Babakova <taytynovak@mail.ru>
2022-09-04 22:38:52 +0700

    Исправленная задача на максимальное значение из трех

--- Second/Program.cs
+++ Second/Program.cs
@@ -1,4 +1,4 @@
-﻿Console.WriteLine("Ведите три числа ");
+﻿Console.WriteLine("Введите три числа ");
 int number1 = int.Parse(Console.ReadLine());
 int number2 = int.Parse(Console.ReadLine());
 int number3 = int.Parse(Console.ReadLine());
