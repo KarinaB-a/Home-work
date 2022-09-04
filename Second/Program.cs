205000a3bfb8e169f92f1a42c0dbf785ab810de2
Karina Babakova <taytynovak@mail.ru>
2022-09-04 22:35:03 +0700

    Максимальное число из трех

---
+++ Second/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Ведите три числа ");
+int number1 = int.Parse(Console.ReadLine());
+int number2 = int.Parse(Console.ReadLine());
+int number3 = int.Parse(Console.ReadLine());
+int max = number1;
+if(max < number2)
+{
+    max = number2;
+}
+if(max < number3)
+{
+    max = number3;
+}
+Console.WriteLine($"Максимальное число {max}");
