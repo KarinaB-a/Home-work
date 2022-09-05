477b977111fd57805bdbff25e29b549105996456
Karina Babakova <taytynovak@mail.ru>
2022-09-05 12:14:53 +0700

    Сохранение gitignore

--- Fourth/Program.cs
+++ Fourth/Program.cs
@@ -1,10 +1,8 @@
 ﻿Console.WriteLine("Введите число ");
-int N = int.Parse(Console.ReadLine()); 
-Console.WriteLine($"Все четные числа от 1 до {N}:");
-for (int A = 1; A <= N; A++)
+int n = int.Parse(Console.ReadLine()); 
+Console.WriteLine($"Все четные числа от 1 до {n}:");
+for (int a = 2; a <= n; a = a + 2)
 {
-    if(A%2 == 0)
-    {
-        Console.Write($"{A} ");
-    }
+    Console.Write($"{a} "); 
 }
+Console.WriteLine();
